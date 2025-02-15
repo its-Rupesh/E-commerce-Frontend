3aa39a882c35eb861cd26263ccf640489e1c2c69
its-Rupesh <rupeshchimankare2004@gmail.com>
2025-02-15 12:22:25 +0530

    Header Page Created

---
+++ src/components/header.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import {
+  FaSearch,
+  FaShoppingBag,
+  FaSignInAlt,
+  FaSignOutAlt,
+  FaUser,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const user = { _id: "1", role: "admin" };
+
+const Header = () => {
+  const [isOpen, setisOpen] = useState<boolean>(false);
+  const logoutHandler = () => {
+    setisOpen(false);
+  };
+  return (
+    <nav className="header">
+      <Link onClick={() => setisOpen(false)} to={"/"}>
+        Home
+      </Link>
+      <Link onClick={() => setisOpen(false)} to={"/search"}>
+        <FaSearch />
+      </Link>
+      <Link onClick={() => setisOpen(false)} to={"/cart"}>
+        <FaShoppingBag />
+      </Link>
+      {user?._id ? (
+        <>
+          <button onClick={() => setisOpen((prev) => !prev)}>
+            <FaUser />
+          </button>
+          <dialog open={isOpen}>
+            <div>
+              {user.role == "admin" && (
+                <Link onClick={() => setisOpen(false)} to={"admin/dashboard"}>
+                  Admin
+                </Link>
+              )}
+              <Link onClick={() => setisOpen(false)} to={"/orders"}>
+                Orders
+              </Link>
+              <button onClick={logoutHandler}>
+                <FaSignOutAlt />
+              </button>
+            </div>
+          </dialog>
+        </>
+      ) : (
+        <Link to={"/login"}>
+          <FaSignInAlt />
+        </Link>
+      )}
+    </nav>
+  );
+};
+
+export default Header;
